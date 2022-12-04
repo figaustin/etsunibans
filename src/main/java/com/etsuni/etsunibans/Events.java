398b238ab00c87e7c13266ebe4eb2ddfdbca0960
Osvaldo Figueroa <figaustin@yahoo.com>
2022-12-04 16:50:01 -0500

    new way to check if player should be allowed to join server

--- src/main/java/com/etsuni/etsunibans/Events.java
+++ src/main/java/com/etsuni/etsunibans/Events.java
@@ -11,13 +11,6 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.Inventory;
 
-import java.text.DateFormat;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -35,35 +28,11 @@ public class Events implements Listener {
     public void onJoin(PlayerLoginEvent event) {
         Player player = event.getPlayer();
         UUID uuid = player.getUniqueId();
-        if(isBanned(uuid.toString())){
+        Unban unban = new Unban(plugin);
+        if(!unban.allowPlayerToJoin(uuid.toString())){
             event.disallow(PlayerLoginEvent.Result.KICK_BANNED, getBanReason(uuid.toString()));
         }
 
-    }
-
-    private boolean isBanned(String uuid) {
-        LocalDateTime now = LocalDateTime.now();
-
-        Document find = new Document("uuid", uuid.toString());
-        FindIterable<Document> finds = plugin.getCollection().find(find);
-        if(finds == null) {
-            return false;
-        }
-
-        for(Document doc : finds) {
-            Bukkit.broadcastMessage(doc.getList("bans", Document.class).get(0).getString("reason"));
-            List<Document> list = doc.getList("bans", Document.class);
-            Date date = list.get(list.size() - 1).getDate("expiration");
-            System.out.println("Date string" + date.toString());
-            LocalDateTime convert = date.toInstant().atOffset(ZoneOffset.UTC).toLocalDateTime();
-            System.out.println("Convert string" + convert.toString());
-            if(now.isAfter(convert)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-        return false;
     }
 
     private String getBanReason(String uuid) {
