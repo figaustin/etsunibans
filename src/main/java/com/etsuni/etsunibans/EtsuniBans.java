6ef1657a5a3b74ba9a35e970d48810957b6b060c
Osvaldo Figueroa <figaustin@yahoo.com>
2022-12-04 19:23:09 -0500

    add config and error handling for null values

--- src/main/java/com/etsuni/etsunibans/EtsuniBans.java
+++ src/main/java/com/etsuni/etsunibans/EtsuniBans.java
@@ -5,7 +5,15 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 
 public final class EtsuniBans extends JavaPlugin {
@@ -14,9 +22,21 @@ public final class EtsuniBans extends JavaPlugin {
     private MongoDatabase database;
     private MongoCollection<Document> collection;
 
+    private File customConfigFile;
+    private FileConfiguration customConfig;
+
+    private static final Logger log = Logger.getLogger("Minecraft");
+
     @Override
     public void onEnable() {
-        connect();
+        createCustomConfig();
+        if(!connect()) {
+            log.severe(String.format("[%s] - Disabled due to config not setup correctly, please add the correct values! " +
+                    "Please change uri to your own uri/connection string!", getDescription().getName()));
+            log.severe(String.format("[%s] - If you get another error after setting this up, you have entered info wrong!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
         this.getCommand("ban").setExecutor(new BanEvent(this));
         this.getServer().getPluginManager().registerEvents(new Events(this), this);
         this.getCommand("history").setExecutor(new HistoryGUI(this));
@@ -29,14 +49,49 @@ public final class EtsuniBans extends JavaPlugin {
 
     }
 
-    public void connect() {
-        mongoClient = MongoClients.create("mongodb://localhost:27017");
-        database = mongoClient.getDatabase("etsunibans");
-        collection = database.getCollection("bans");
+    public Boolean connect() {
+        String uri = customConfig.getString("database.uri");
+        String databaseName = customConfig.getString("database.database_name");
+        String collectionName = customConfig.getString("database.collection_name");
+
+        if(uri == null) {
+            return false;
+        }
+        if(databaseName == null) {
+            return false;
+        }
+        if(collectionName == null) {
+            return false;
+        }
+        mongoClient = MongoClients.create(uri);
+        database = mongoClient.getDatabase(databaseName);
+        collection = database.getCollection(collectionName);
+        return true;
     }
 
     public MongoCollection<Document> getCollection() {
         return collection;
+    }
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "config.yml");
+        if(!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+
+        customConfig = new YamlConfiguration();
+
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
     }
 
 }
